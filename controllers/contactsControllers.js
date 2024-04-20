391103946f5856b51e622e12c2a78e633d403a10
koshmaniuk <trident93@gmail.com>
2024-04-20 00:18:29 -0400

    update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,89 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+  updateContactById,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const data = await listContacts();
+    res.status(200).json({
+      msg: "success",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await getContactById(id);
+    if (!data) {
+      throw HttpError(404);
+    }
+    res.status(200).json({
+      msg: "success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await removeContact(id);
+    if (!data) {
+      throw HttpError(404);
+    }
+    res.status(200).json({
+      msg: "succes",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
 
-export const updateContact = (req, res) => {};
+    const { name, email, phone } = req.body;
+    const data = await addContact(name, email, phone);
+    res.status(201).json({
+      msg: "created",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
+    const { id } = req.params;
+    const data = await updateContactById(id, req.body);
+    if (!data) throw HttpError(404);
+    console.log(data);
+    res.status(200).json({
+      msg: "updated",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
